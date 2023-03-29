05d882aa157ec7f3085fce583bbc535773e737d5
Codigoquatro <113838988+Codigoquatro@users.noreply.github.com>
2023-03-29 12:27:21 -0300

    estruturando Model (#1)
    * estruturando Model
    
    * criado controller e request
    
    * criar controller e request

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tarefa extends Model
 {
-    use HasFactory;
+    protected $table = 'tarefa';
+    protected $primarykey = 'id_tarefa';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'responsavel',
+        'descricao',
+        'data_conclusao'
+    ];
+
+    protected $guarded = [];
+
 }
