76fb58e39efd92aa7e3a763a22b56769a6dd4eae
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-02 15:34:11 -0300

    criado controller

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use fabricainfo\Tarefa;
+use Illuminate\Support\Facedes\Redirect;
+use fabricainfo\Http\Requests\TarefaFromRequest;
+use DB;
 
 class TarefaController extends Controller
 {
+    public function __construct(){
+        //
+    }
 
+    public function index(Request $request){
+
+        if($request){
+            $query=trim($request->get('seachText'));
+            $tarefa=DB::table('tarefa')
+            ->where('responsavel','LIKE', '%'.$query.'%')
+            ->where('descricao','=','1')
+            ->orderBy('id_tarefa','desc')
+            ->paginate('7');
+            return view('tarefa',[
+                "tarefa"=>$tarefa, "searchText"=>$query
+            ]);
+        }
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+
+    }
 }
