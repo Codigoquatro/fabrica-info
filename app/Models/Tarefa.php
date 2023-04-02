3a83502d647bd6a11009125b6153070468b63556
Raynara <codigoquatro2022@gmail.com>
2023-04-02 15:25:31 -0300

    ajuste no Model

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Tarefa extends Model
 {
     use HasFactory;
+    protected $table = 'tarefa';
+    protected $primaryKey = 'id_tarefa';
+
+    public $timestamps = false;
+    protected $fillable = [
+    'id_tarefa',
+    'responsavel',
+    'descricao',
+    'data_conclusao'    
+    ];
+
+    protected $guarded = [];
 }
