da7b39f98097acdc02d0374c17434a3d17af62ec
Raynara <codigoquatro2022@gmail.com>
2023-04-02 18:37:19 -0300

    ajuste

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.admin')
 @section('conteudo')
 <div class="row">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		<h3>Lista de Tarefas <a href="categoria/create"><button class="btn btn-success">Novo</button></a></h3>
+		@include('estoque.categoria.search')
+	</div>
+</div>
+<div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<div class="table-responsive">
 			<table class="table table-striped table-bordered table-condensed table-hover">
