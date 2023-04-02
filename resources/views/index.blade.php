c9011ad24e1a2f981e795bd38abf19b5a5c96964
Raynara <codigoquatro2022@gmail.com>
2023-04-02 18:39:49 -0300

    ajuste

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 		<h3>Lista de Tarefas <a href="categoria/create"><button class="btn btn-success">Novo</button></a></h3>
-		@include('estoque.categoria.search')
+		@include('search')
 	</div>
 </div>
 <div class="row">
@@ -23,15 +23,14 @@
 					<td>{{ $ta->descricao}}</td>
                     <td>{{ $ta->data_conclusao}}</td>
 					<td>
-						<a href="{{URL::action('TarefaController@edit',$cat->id_tarefa)}}"><button class="btn btn-info">Editar</button></a>
-                         <a href="" data-target="#modal-delete-{{$cat->id_tarefa}}" data-toggle="modal"><button class="btn btn-danger">Excluir</button></a>
+						<a href="#"><button class="btn btn-info">Editar</button></a>
+                         <a href="#" data-target="#" data-toggle="modal"><button class="btn btn-danger">Excluir</button></a>
 					</td>
 				</tr>
-				@include('modal')
-				@endforeach
+
 			</table>
 		</div>
-		{{$tarefas->render()}}
+	
 	</div>
 </div>
 @stop
