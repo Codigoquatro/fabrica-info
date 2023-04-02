ad1ab7816e51c2baf05b05756d451be66e7525ba
Raynara <codigoquatro2022@gmail.com>
2023-04-02 19:23:55 -0300

    ajuste index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
 	</div>
 </div>
 @stop
+@section('conteudo')
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<div class="table-responsive">
@@ -34,4 +35,4 @@
 	
 	</div>
 </div>
-
+@stop
