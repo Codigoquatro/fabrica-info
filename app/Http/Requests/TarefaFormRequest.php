0617b8fa530935d6b79d9746266058f4394a2b27
Raynara <codigoquatro2022@gmail.com>
2023-04-02 16:17:12 -0300

    ajuste

--- app/Http/Requests/TarefaFormRequest.php
+++ app/Http/Requests/TarefaFormRequest.php
@@ -13,7 +13,7 @@ class TarefaFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class TarefaFormRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'responsavel'=>'required|max:256',
+            'descricao'=>'max:256',
+            'data_conclusao'=>'max:256',
         ];
     }
 }
