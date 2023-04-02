bc4b5cce461b6822bd58ccfa55c04845b4da99f6
Raynara <codigoquatro2022@gmail.com>
2023-04-02 19:26:00 -0300

    ajuste index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.admin')
-@section('conteudo')
+
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 		<h3>Lista de Tarefas <a href="create"><button class="btn btn-success">Novo</button></a></h3>
         @include('search')	
 	</div>
 </div>
-@stop
+@extends('layouts.admin')
+@section('conteudo')
 @section('conteudo')
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
