e87301b7a10868e50b42a86543086965ca540830
Raynara <codigoquatro2022@gmail.com>
2023-04-02 20:23:53 -0300

    ajuste index

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(TarefaController::class)->group(function () {
     Route::get('/', 'index');
-    Route::post('/novo', 'create');
+    Route::get('/novo', 'create');
 });
 
