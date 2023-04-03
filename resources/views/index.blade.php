e5f8a22f1d8f7bcec46a2af676ecebd583331738
Raynara <codigoquatro2022@gmail.com>
2023-04-02 23:11:50 -0300

    ajuste

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('conteudo')
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-		<h3>Lista de Tarefas <a href="{{route('create')}}"><button class="btn btn-success">Novo</button></a></h3>
+		<h3>Lista de Tarefas <a href="App/resources/view/create.blade.php"><button class="btn btn-success">Novo</button></a></h3>
 	</div>
 
 </div>
