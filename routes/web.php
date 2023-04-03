554bf3e4ae68b1bf2d9118374e94fe6d0bcece24
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-02 21:17:19 -0300

    atualizacao

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::controller(TarefaController::class)->group(function () {
     Route::post('/novo', 'store');
 });
 */
-Route::resource('index', 'TarefaController');
+Route::resource('tarefa', 'TarefaController');
