966d55ddd17e8e54864e93e46dddeb8e6f771ebb
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-03 13:12:55 -0300

    criado arquivos para o view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
                     <td>{{ $cat->data_conclusao}}</td>
 					<td>
                         <a href="/tarefa/{{$cat->id_tarefa}}/edit" class="btn btn-sm btn-primary">Editar</a>
-                        <a href="/tarefa/{{$cat->id_tarefa}}" class="btn btn-sm btn-danger">Apagar</a>
+                        <a href="/tarefa/{{$cat->id_tarefa}}/destroy" class="btn btn-sm btn-danger">Apagar</a>
 					</td>
 				</tr>
 
