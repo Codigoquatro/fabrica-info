3ad4edbef2660ca2b648fd449c4f3a46152b4994
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-02 22:35:20 -0300

    atualizacao

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,9 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 		<h3>Lista de Tarefas <a href="tarefa.create"><button class="btn btn-success">Novo</button></a></h3>
+        @include('search')
 	</div>
-		
+
 </div>
 
 
@@ -19,7 +20,7 @@
 					<th>Descrição</th>
 					<th>Data Conclusão</th>
 				</thead>
-              
+
 				<tr>
 					<td></td>
 					<td></td>
@@ -33,7 +34,7 @@
 
 			</table>
 		</div>
-	
+
 	</div>
 </div>
 @stop
