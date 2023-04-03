7d190b9426324a9d8e94f1be3f7d7e0d98839596
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-03 06:38:36 -0300

    creado controler

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [TarefaController::class, 'index']);
+
+
+Route::resource('tarefa',TarefaController::class)
+->middleware(['auth','verified']);
+
 
 
 
