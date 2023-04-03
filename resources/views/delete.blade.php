a4ffaa77b1ec1a3b38d5978404d7dff5339621b0
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-03 10:35:50 -0300

    creado controler

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -1,1 +1,28 @@
+@extends('layouts.admin')
+@section('conteudo')
+<div class="card border">
+    <div class="card-body">
+        <form action="/categorias/{{$cat->id}}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="Responsavel">Responsável</label>
+                <input type="text" class="form-control" name="Responavel" value="{{$cat->responsavel}}"
+                       id="Responsavel" placeholder="Responsavel">
+            </div>
 
+            <div class="form-group">
+                <label for="Descricao">Descrição</label>
+                <input type="text" class="form-control" name="descricao" value="{{$cat->descricao}}"
+                       id="descricao" placeholder="Descricao">
+            </div>
+
+            <div class="form-group">
+                <label for="data_conclusao">Data Conclusão</label>
+                <input type="text" class="form-control" name="data_conclusao" value="{{$cat->data_conclusao}}"
+                       id="data_conclusao" placeholder="data_conclusao">
+            </div>
+            <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
+            <button type="cancel" class="btn btn-danger btn-sm">Cancel</button>
+        </form>
+    </div>
+</div>
