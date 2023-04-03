8da5f7349a6c86f3021d5040b3fd5f90387a7b88
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-03 08:15:16 -0300

    creado controler

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 		<h3>Lista de Tarefas <a href="tarefa/create"><button class="btn btn-success">Novo</button></a></h3>
-        @include('search')
+
 	</div>
 
 </div>
