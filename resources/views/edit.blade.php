a4e1bf6689c5fcea8b7bd1e1f6022f07561e8cb1
Codigoquatro <codigoquatro2022@gmail.com>
2023-04-03 09:59:18 -0300

    creado controler

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,9 +5,21 @@
         <form action="/categorias/{{$cat->id}}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="nomeCategoria">Nome da Categoria</label>
-                <input type="text" class="form-control" name="nomeCategoria" value="{{$cat->responsavel}}"
-                       id="nomeCategoria" placeholder="Categoria">
+                <label for="Responsavel">Responsável</label>
+                <input type="text" class="form-control" name="Responavel" value="{{$cat->responsavel}}"
+                       id="Responsavel" placeholder="Responsavel">
+            </div>
+
+            <div class="form-group">
+                <label for="Descricao">Descrição</label>
+                <input type="text" class="form-control" name="descricao" value="{{$cat->descricao}}"
+                       id="descricao" placeholder="Descricao">
+            </div>
+
+            <div class="form-group">
+                <label for="data_conclusao">Data Conclusão</label>
+                <input type="text" class="form-control" name="data_conclusao" value="{{$cat->data_conclusao}}"
+                       id="data_conclusao" placeholder="data_conclusao">
             </div>
             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
             <button type="cancel" class="btn btn-danger btn-sm">Cancel</button>
